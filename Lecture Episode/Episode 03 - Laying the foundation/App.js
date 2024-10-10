b2f42ec2f982eaf981812b16556e5121457cc879
armanali485 <armanaligithub@gmail.com>
2024-10-10 18:56:49 +0530

    Episode 03 - Laying the foundation

--- Lecture Episode/Episode 03 - Laying the foundation/App.js
+++ Lecture Episode/Episode 03 - Laying the foundation/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-// React Element
+//React Element root
 const heading = React.createElement("h1",{id:"heading"},"I'm React Elemet 🚀")
 console.log(heading)
 
 
-//JSX
-const jsxHeading=<h1>I'm from JSX</h1>
+//React Element
+const jsxHeading=(<h1 id="heading">
+    I'm React Element
+    </h1>);
 console.log(jsxHeading);
-
-
-
-
+//React Component
+const ReactComponent=()=>{
+   
+    return (<div>
+        {jsxHeading}
+        <h1>I'm React Component</h1></div>);
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxHeading);
+root.render(<ReactComponent/>);
